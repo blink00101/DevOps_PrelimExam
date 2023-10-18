7e5a87d637e3f968d5a6676ee62bcf046a54d56d
blink00101 <valerososarahmae@gmail.com>
2023-10-18 08:50:50 +0000

    additional

---
+++ grade.py
@@ -0,0 +1,10 @@
+import unittest
+def check_Grade():
+       return x >= 50
+
+class Grade_check(unittest.TestCase):
+      def grade(self):
+        self.assertTrue(grade(10))
+
+if __name__ == '__main__':
+    unittest.main()
