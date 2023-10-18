bf82cc78a317fdbdc58171b48d4720c73af4c0c1
blink00101 <valerososarahmae@gmail.com>
2023-10-18 08:36:08 +0000

    Prelim Exam

---
+++ Pedro.py
@@ -0,0 +1,10 @@
+import unittest
+def name_check():
+   return name == ('PEDRO')
+
+class name_Check(unittest.TestCase):
+    def name_Verified(self):
+        self.assertTrue(name_Verified('Pedro'))
+
+if __name__ == '__main__':
+    unittest.main()
